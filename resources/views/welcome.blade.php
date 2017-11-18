21b0575ff7e8c85ea8973603b2ff4dd5f3f18d93
Mohammed Jebrini <mohammed.jebrini@gmail.com>
2017-11-18 13:51:10 +0200

    update the main view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>محلّي - Mahalli Portal</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -69,25 +69,22 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="http://mahalli.co">الرئيسية</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('login') }}">دخول</a>
+                        <a href="{{ route('register') }}">تسجيل</a>
                     @endauth
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <img height="200" src="http://mahalli.co/wp-content/uploads/2017/11/Logo.png" />
                 </div>
-
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ route('register') }}">سجل متجرك</a>
+                    <a href="{{ route('login') }}">دخول</a>
+                    <a href="http://mahalli.co/">عن محلي</a>
                 </div>
             </div>
         </div>
